c58220ea0c9580f6232fdb1d0a3f9f78c8536836
Roman <romaactor@gmail.com>
2021-09-14 19:01:35 +0300

    new features

--- sources/public_ip.py
+++ sources/public_ip.py
@@ -2,6 +2,10 @@ import subprocess
 from .colors import bcolors
 
 def PublicIP():
-    ip_raw = subprocess.check_output('dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com', shell=True, encoding='utf-8')
+    try:
+        ip_raw = subprocess.check_output('dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com', shell=True, encoding='utf-8')
+    except CalledProcessError:
+        print("Error. Please check your interrnet connection.")
+        exit()
     print('\n', f"{bcolors.OKBLUE}Public IP address:{bcolors.ENDC}\n", ip_raw.replace('"', ""))
     exit()
